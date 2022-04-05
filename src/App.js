58227a00cd256500ed7fb8710f87c6437efb9ff0
youngah lee <dudgk0216@naver.com>
2022-04-06 04:01:42 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,135 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import 'react-bootstrap';
+import { AiFillHome } from 'react-icons/ai';
+import { BsMessenger} from 'react-icons/bs';
+import { GiWrappedHeart } from 'react-icons/gi';
+import {Route, Router, Routes} from 'react-router-dom'
+import Update from './Update';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+
+
+
+
+
+function App(props) {
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="App background">
+      <header className='container header-size'>
+        <div className='ui-flex row'>
+          <div className='logo-position'>  
+            <img className='ui-set s4Iyt' alt="Instagram"  src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" >
+       </img></div>
+      
+       <Update></Update>
+       
+       
+       
+       <div>
+       
+       <GiWrappedHeart className='iposition'  size="30"
+       onClick={()=>{console.log('짜잔')}}></GiWrappedHeart>
+     
+      <BsMessenger className='iposition '  size="24"
+      onClick={()=>{console.log('짜잔')}}></BsMessenger>
+      
+      <AiFillHome className='iposition '  size="30"
+      onClick={()=>{console.log('짜잔')}}></AiFillHome>   
+     
+       </div>
+        </div>
       </header>
+      
+   
+      <Routes>
+      
+      <Route exact path='/' element={<Main></Main>}> 
+        </Route>
+      </Routes>
+    
     </div>
   );
 }
 
+
+const FollwerList =({follower})=>{
+   
+
+  return <div className='follower-location'>
+    <div> 나를 팔로우하는사람들</div>
+    {
+      follower.map((item)=>{
+        return(
+         
+          <div>
+             
+            <img className='follower-size follwer-location'alt={item.login} src={item.avatar_url}></img>
+           {item.login}
+
+                 
+          </div>
+         
+        )
+      })
+    }
+    {/* <Pagination></Pagination> */}
+  </div>
+}
+
+const Followers = (props)=>{
+  let [follower,followerChange] = useState([]);
+  
+      useEffect(()=>{
+        axios.get('https://api.github.com/users/john-smilga/followers?per_page=100').
+        then((result)=>{
+          console.log('팔로우',result.data);
+          let copy=  [...result.data]
+         followerChange(copy)
+        
+         
+          
+        }).catch(()=>{
+     
+          console.log('실패')
+        },);
+
+      },[]);
+     
+  
+
+    return (
+     <div>
+       <FollwerList follower={follower}></FollwerList>
+     </div>
+
+    )
+ 
+
+}
+
+// const Pagination = ({follower})=>{
+
+//   return<div>
+//   <Followers></Followers>
+//   </div> 
+// }
+
+function Main(props){
+  
+  return <div className='main-size container'>
+    <div>
+    <Followers></Followers>
+    
+    </div>
+   
+     <span>
+  여기는 피드
+</span>
+ 
+    </div>
+
+}
 export default App;
---
+++ src/Update.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Button } from 'react-bootstrap';
+
+function Update(){
+    return <div className="d-grid gap-2">
+    <Button variant="primary update-button " size="lg" 
+
+>
+      Block level button
+    </Button>
+   
+  </div>
+}
+
+export default Update
